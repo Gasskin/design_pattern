7ecd0957d43f68260e36ba9887f8ab06eacdf7c2
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-29 19:50:03 +0800

    装饰者模式

---
+++ 设计模式/2.4装饰模式.cpp
@@ -0,0 +1,72 @@
+#include "iostream"
+using namespace std;
+
+//抽象产品
+class Component
+{
+public:
+	Component() {}
+	virtual void operation() = 0;
+};
+
+//具体产品
+class Phone :public Component
+{
+public:
+	Phone() {}
+	virtual void operation()override
+	{
+		cout << "打电话" << endl;
+	}
+};
+
+//抽象装饰者
+class Decorator :public Component
+{
+public:
+	Decorator() {}
+	virtual void operation()override
+	{
+		this->component->operation();
+	}
+	Component* getComponent()
+	{
+		return component;
+	}
+	void setComponent(Component* c)
+	{
+		this->component = c;
+	}
+private:
+	Component* component;
+};
+
+//具体装饰者
+class Shell :public Decorator
+{
+public:
+	Shell(Component* c)
+	{
+		this->setComponent(c);
+	}
+	void operation()
+	{
+		this->getComponent()->operation();
+		newOperation();
+	}
+	void newOperation()
+	{
+		cout << "装手机壳" << endl;
+	}
+};
+
+
+int main()
+{
+	Component* c = new Phone();
+	c->operation();
+	cout << "---" << endl;
+	Component* cplus = new Shell(c);
+	cplus->operation();
+	return 0;
+}
